5365d5e3b75e4fd5eff3789f2b5356e8fb4afb38
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-05-20 17:15:41 +0000

    [ADD] Code for Login added

--- app.py
+++ app.py
@@ -42,8 +42,28 @@ def contact():
 
 
 # Login page
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        return redirect(url_for("books"))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
