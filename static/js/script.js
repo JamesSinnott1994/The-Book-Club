d498bd7a69727880fa80c59a78315b3077fadae5
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-05-25 16:41:45 +0000

    [ADD] Call-To-Action buttons for Home page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
     // Sidenav
     $('.sidenav').sidenav();
+
+    // Resize event, for making paragraph text the same height
+    // (Keeps CTA buttons at the one level)
+    // https://www.techiedelight.com/trigger-window-resize-event-javascript/
+    $(window).trigger('resize');
+    $(window).resize(makeParagraphHeightSame);
+
+    // Make height of CTA (Call-To-Action) paragraphs the same
+    function makeParagraphHeightSame() {
+        let maxHeight = 0;
+
+        $(".call-to-action-area p").each((i, p) => {
+            if ($(p).height() > maxHeight) { 
+                maxHeight = $(p).height();
+            }
+        });
+
+        $(".call-to-action-area p").height(maxHeight);
+    }
 });
