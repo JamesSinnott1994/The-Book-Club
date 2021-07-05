cffc40f891732e05ed111d38525fddab5b315b7d
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-07-05 11:20:36 +0000

    [REFACTOR[ Debug mode turned off

--- app.py
+++ app.py
@@ -588,4 +588,4 @@ def delete_review(book_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
